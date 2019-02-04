26512e4c69f59edb89f252730f695cb9eeb6ceb1
Gelline Gruta <gelline.gruta@shoresuite.com>
2019-02-04 11:41:20 +0800

    Hello World!

--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
 
+class AppTest extends Component{
+  constructor(){
+    super();
+
+    this.state = {
+      sayHello: 'Hello World',
+    };
+
+    this.buttonChange = this.buttonChange.bind(this);
+  }
+
+  buttonChange(){
+    this.setState({sayHello: 'Good Bye!'})
+  }
+
+  render(){
+    return(
+      <div className="App"> 
+        <p>{this.state.sayHello}</p>
+        <button onClick={this.buttonChange}>Bye Bye!</button>
+      </div>
+    );
+  }
+}
+
 class App extends Component{
 
   constructor(){
@@ -8,13 +33,15 @@ class App extends Component{
 
     this.state = {
       currRates: [],
-      value : 'Select',
+      selectValue : 'None',
+      inputValue: 0,
     };
 
-    this.change = this.change.bind(this); //ALWAYS BIND THIS
+    this.selectChange = this.selectChange.bind(this); //ALWAYS BIND THEM FUNCTIONS?
+    this.inputChange = this.inputChange.bind(this);
   }
 
-
+  //The things inside the select Dropdown
   componentDidMount(){
     const API_KEY="d69d63eccfc65d52d99f5200bbf3dbf4";
     const apiGet= "http://data.fixer.io/api/latest?access_key=" + API_KEY;
@@ -29,7 +56,7 @@ class App extends Component{
       console.log(theRates);
       let optionOutput = Object.entries(theRates).map(([optionKey, optionValue])=>{
         return(
-            <option value={optionValue} className={optionKey.toString()}>{optionKey.toString()}</option> 
+          <h1></h1>
         )
       })
       console.log(optionOutput);
@@ -39,23 +66,29 @@ class App extends Component{
     
   }
 
-  change(event){
-    this.setState({value: event.target.value});
+  selectChange(event){
+    this.setState({selectValue: event.target.value});
+  }
+
+  inputChange(event){
+    this.setState({inputValue: event.target.value});
   }
 
   render() {
     return (
       <div className="App"> 
-        EUR: <input type="number"/> to&nbsp;
-        <select onChange={this.change} value={this.state.value}>
-          <option value='select'>Select Currency</option>
+        EUR: <input type="number" onChange={this.inputChange} value={this.state.inputValue}/> to&nbsp;
+        
+        <select onChange={this.selectChange} value={this.state.selectValue}>
+          <option value='None'>Select Currency</option>
           {this.state.currRates}
         </select>
-        &nbsp;{this.state.value}
-     
+
+        &nbsp;{this.state.selectValue} multiply with {this.state.inputValue}
+        &nbsp; is equals to {this.state.selectValue * this.state.inputValue}
       </div>
     );
   }
 }
 
-export default App;
+export default AppTest;
